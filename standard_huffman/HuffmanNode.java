3e4c22e60f500c93d0b1c20b71aa6e05c18e83ac
hossamelgendy1 <59685428+hossamelgendy1@users.noreply.github.com>
2021-12-06 08:59:29 +0200

    Added implementation

---
+++ standard_huffman/HuffmanNode.java
@@ -0,0 +1,10 @@
+package standard_huffman;
+
+public class HuffmanNode {
+
+	int frequency;
+    char c;
+
+    HuffmanNode left;
+    HuffmanNode right;
+}
---
+++ standard_huffman/MyComparator.java
@@ -0,0 +1,11 @@
+package standard_huffman;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<HuffmanNode> {
+    public int compare(HuffmanNode x, HuffmanNode y)
+	{
+
+		return x.frequency - y.frequency;
+	}
+}
